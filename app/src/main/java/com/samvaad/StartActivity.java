d47183eb22b6be1a84126b59197f9efb285551a0
Manish Kumar <search.manish16@gmail.com>
2018-01-30 17:59:07 +0530

    Registration  completed

--- app/src/main/java/com/samvaad/StartActivity.java
+++ app/src/main/java/com/samvaad/StartActivity.java
@@ -1,13 +1,25 @@
 package com.samvaad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity{
+    private Button mRegBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        mRegBtn= findViewById(R.id.start_reg_btn);
+        mRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent reg_intent =new Intent(StartActivity.this,RegisterActivity.class);
+                startActivity(reg_intent);
+            }
+        });
     }
 }
